0d7f8ddced223ea1c3d88f5b0d388541ad215e90
Guanghui Li <154851122+BUAALGH@users.noreply.github.com>
2025-12-12 14:43:42 +0800

    Create scanning_parameters.m

---
+++ reconstruction_codes/scanning_parameters.m
@@ -0,0 +1,12 @@
+%% Scanning parameters setup
+
+% rotation parameters
+angle_sequence = 0:6:180;   % angle sequence of FFL rotations
+
+% driven field parameters
+ff = 1;         % scanning frequency           
+fd = 3000;      % excitation frequency     
+
+% sampling parameters
+Fs = 3e6;           % sampling frequency
+Sampling_time = 1;  % sampling time = 1s
